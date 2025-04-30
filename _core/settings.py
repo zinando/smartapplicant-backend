895b234c468cfd430100f44e2be1bfece2b96b77
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2025-04-30 16:22:25 +0100

    Edit cors header settings

--- _core/settings.py
+++ _core/settings.py
@@ -8,7 +8,7 @@ import resource
 # Load environment variables from .env file
 load_dotenv()
 
-print(f"Password: {os.getenv('POSTGRES_PASSWORD')}")
+# print(f"Password: {os.getenv('POSTGRES_PASSWORD')}")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
