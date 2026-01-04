2a03f6eee6a8f8f52fd938e4ca31f4e10f0424d2
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2026-01-04 14:20:27 +0100

    Create a command for managing pending requests

--- automation/management/commands/show_pending_requests.py
+++ automation/management/commands/show_pending_requests.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from automation.helpers import get_pending_requests
+from django.conf import settings
 
-request_key = ''
+request_key = f"{settings.SMARTAPPLICANT.get('WABA_PHONE_NUMBER_ID')}_{str(2347031104270)}_pending_requests"
 
 class Command(BaseCommand):
     help = "Show pending requests"
@@ -15,6 +16,6 @@ class Command(BaseCommand):
         for request in pending_requests:
             self.stdout.write(f"""
                 Pending Request: {request}
-                
+
                 <<<----------------------------END----------------------------------->>>\n\n
             """)
