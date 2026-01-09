c29c9248cec768b7251585385db8d2a268dce15d
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2026-01-09 17:59:47 +0100

    Troubleshoot autoposting error

--- automation/content_automation/facebook.py
+++ automation/content_automation/facebook.py
@@ -18,13 +18,13 @@ class AutomateFacebookPost:
 
     def __init__(self, page_id):
         self.page_id = page_id
-        self.__page_access_token = settings.PAGE_DATA.get(page_id, {}).get("access_token", "")
         self.__post_url = self.__post_url.replace("page_id", str(page_id))
         # self.__business_info = Tenant.objects.filter(fb_page_id=page_id).first()
         self.__business_info = AutomatedClients.objects.filter(platform="facebook", client_id=page_id).first()
         if not self.__business_info:
             logger.warning(f"No business info found for page_id: {page_id}")
             raise ValueError("Invalid page_id or business info not found. Ensure the business that owns this page is registered with us.")
+        self.__page_access_token = settings.PAGE_DATA.get(page_id, {}).get("access_token", "") or self.__business_info.page_access_token
         if not self.__page_access_token:
             logger.warning(f"No access token found for page_id: {page_id}")
             raise ValueError("Access token not found for the given page_id.")
