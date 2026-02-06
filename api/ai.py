3f773514a93cd7d15ecdbbe1ace72714efe97036
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2026-02-06 05:44:37 +0100

    Troubleshoot automation errors

--- api/ai.py
+++ api/ai.py
@@ -566,7 +566,8 @@ def get_structured_data_from_gemini_smart(prompt: str):
         text = match.group(0)
         text = text.replace('```json', '')
         text = text.replace('```', '')
-        response = json.loads(text)
+        # response = json.loads(text)
+        response = text
     except Exception as e:
         print(f'Error parsing Gemini response: {e}')
         message = f'Gemini response: {e}'
